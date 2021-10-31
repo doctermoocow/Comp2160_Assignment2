e0de169c6070335c5f8e70601ff79d04a3aec3a5
Oakaidden <54323962+Oakaidden@users.noreply.github.com>
2021-11-01 03:24:30 +1100

    Update Checkpoints.cs

--- Comp 2160 Assignment 2/Assets/Scripts/Checkpoints.cs
+++ Comp 2160 Assignment 2/Assets/Scripts/Checkpoints.cs
@@ -7,7 +7,11 @@ public class Checkpoints : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //Vector3 checkpoints = new Vector3[transform.childCount];
+        Vector3[] checkpoints = new Vector3[transform.childCount];
+        for(int i = 0; i < checkpoints.Length; i++)
+        {
+            checkpoints[i] = transform.GetChild(i).position;
+        }
     }
 
     // Update is called once per frame
