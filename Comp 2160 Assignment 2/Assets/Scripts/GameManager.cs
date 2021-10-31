3666ae43e0d069584b5ffa068598c9ccd5805a6a
Oakaidden <54323962+Oakaidden@users.noreply.github.com>
2021-10-31 18:55:07 +1100

    analytics added

--- Comp 2160 Assignment 2/Assets/Scripts/GameManager.cs
+++ Comp 2160 Assignment 2/Assets/Scripts/GameManager.cs
@@ -1,18 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Analytics;
 
 public class GameManager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        //AnalyticsEvent.GameStart();
+        AnalyticsEvent.GameStart();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void GameOver()
+    {
+        //time since beginning
+        //players position
+        //object player collided with
+        AnalyticsEvent.GameOver();
+    }
+
+    void Checkpoint()
+    {
+        //time since beginning
+        //player health (before checkpoint health boost
     }
 }
