61f65d2c97c702c6e4c6509a9e40f4f37d69d4f5
Oakaidden <54323962+Oakaidden@users.noreply.github.com>
2021-11-02 16:34:06 +1100

    still not working

--- Comp 2160 Assignment 2/Assets/Scripts/PlayerDamage.cs
+++ Comp 2160 Assignment 2/Assets/Scripts/PlayerDamage.cs
@@ -11,12 +11,13 @@ public class PlayerDamage : MonoBehaviour
     private float restore;
 
     private Rigidbody car;
+    private GameManager gameManager;
     
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        maxHealth = gameManager.playerHealth;
     }
 
     // Update is called once per frame
