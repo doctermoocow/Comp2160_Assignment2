2fd0885b10168bcc4fb368944bb1e5398693a74f
Jaiden Ireland 45328455 <jaiden.irealnd@students.mq.edu.au>
2021-11-05 22:23:16 +1100

    Camera fixed

--- Comp 2160 Assignment 2/Assets/Scripts/CameraController.cs
+++ Comp 2160 Assignment 2/Assets/Scripts/CameraController.cs
@@ -6,70 +6,56 @@ public class CameraController : MonoBehaviour
 {
 
     public Rigidbody cameraTarget;
-    public float lerpSpeed = 1; 
     public LayerMask groundLayer;
-    public Transform cameraTransfrom;
-    public float cameraMaxSpeedDifference = 4;
-    public float cameraMaxTurnDifference = 1;
 
 
-    private Vector3 cameraStartingTransform;
 
+    //camera stuff
+    public float scaleFactor = 0.1f;
+    public float difPerSecond = 10f;
+    public float velocityEmphasis = 5f;
+    private Vector3 offset;
+    private Vector3 newOffset;
+    private Vector3 oldOffset;
 
-    // Start is called before the first frame update
     void Start()
     {
-        cameraStartingTransform = cameraTransfrom.position;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        //basic add camera
-       /// transform.position = cameraTarget.position;
+        newOffset = cameraTarget.transform.position;
     }
 
     void LateUpdate()
     {
-        //calculate camera distance from ground (needs to stay before calculating vector
-        Ray ray = new Ray(cameraTarget.transform.position, Vector3.down);
+
+        Debug.DrawLine(cameraTarget.transform.position, cameraTarget.transform.position +
+            (new Vector3(cameraTarget.velocity.x, 0, cameraTarget.velocity.z)), Color.magenta);
+
+
+        Ray ray = new Ray(cameraTarget.position, Vector3.down);
         RaycastHit hit;
-        Vector3 currTargetPosition = transform.position; //if the ray does not hit, the camera still needs a position to lerp from
 
         if (Physics.Raycast(ray, out hit, Mathf.Infinity, groundLayer))
         {
-            currTargetPosition = hit.point;
-          //  transform.position = hit.point;
+            Debug.DrawRay(ray.origin, ray.direction * cameraTarget.velocity.magnitude, Color.white);
         }
-        //camera parent follows car
-        transform.position = currTargetPosition;
 
 
-        //Forwards and backwards camera movement
+        oldOffset = cameraTarget.transform.position;
+        //  print(offset);
+        newOffset += (oldOffset + (cameraTarget.velocity * velocityEmphasis) - newOffset) * Time.fixedDeltaTime / difPerSecond;
 
-        //gets the offset of the velocity rather than local world coordinates and clamps it to the max distance the camera can move
-        float zMax = Mathf.Clamp(cameraTarget.velocity.z,0,cameraMaxSpeedDifference);
+        Vector3 offset = Vector3.Lerp(cameraTarget.position, newOffset, scaleFactor);
+        offset.y = hit.point.y;
 
-        print(zMax);
-        //zMax = Vector3.ClampMagnitude(zMax, cameraMaxSpeedDifference);
+       // print(newOffset);
+
+        transform.position = offset;
 
 
-
-
-        Vector3 cameraNewOffset = new Vector3(0, 1.78f, transform.InverseTransformPoint(cameraStartingTransform).z);//z offset + horizontal offset;
-        cameraTransfrom.transform.localPosition = Vector3.Lerp(transform.InverseTransformPoint(cameraStartingTransform), cameraNewOffset, 1);
-        /*
-
-        //Forwards and backwards camera movement
-        //calculate velocity and lerp between current position and velocity vector
-        float zLerp = Mathf.Lerp(currTargetPosition.z, currTargetPosition.z - cameraMaxSpeedDifference, cameraTarget.velocity.magnitude/10);
-        camera.transform.localPosition = new Vector3(currTargetPosition.x, currTargetPosition.y , zLerp);
-        */
 
         //turning camera movement        
-        transform.eulerAngles = new Vector3(transform.rotation.eulerAngles.x, cameraTarget.transform.eulerAngles.y, transform.rotation.eulerAngles.z);
+       transform.eulerAngles = new Vector3(transform.rotation.eulerAngles.x, cameraTarget.transform.eulerAngles.y, transform.rotation.eulerAngles.z);
 
 
-
+       // previousVelocity = cameraTarget.velocity;
     }
 }
